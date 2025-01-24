73dbbe4a32ccad3a23e50612b129f358a35df0fd
dayong <wangdayong228@163.com>
2025-01-24 15:22:27 +0800

    nil

--- apisix-go-plugin-runner/cmd/go-runner/plugins/resphandler/rpc_resp_handler.go
+++ apisix-go-plugin-runner/cmd/go-runner/plugins/resphandler/rpc_resp_handler.go
@@ -2,6 +2,7 @@ package resphandler
 
 import (
 	"bytes"
+	"compress/flate"
 	"compress/gzip"
 	"encoding/json"
 	"fmt"
@@ -9,6 +10,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/andybalholm/brotli"
 	"github.com/apache/apisix-go-plugin-runner/cmd/go-runner/plugins/count"
 	pkgHTTP "github.com/apache/apisix-go-plugin-runner/pkg/http"
 	"github.com/apache/apisix-go-plugin-runner/pkg/log"
@@ -142,11 +144,16 @@ func readDecompressedBody(w pkgHTTP.Response) ([]byte, error) {
 	// var body []byte
 	var reader io.Reader = bytes.NewReader(rawBody)
 	encoding := w.Header().Get("Content-Encoding")
-	if encoding == "gzip" {
+	switch encoding {
+	case "gzip":
 		reader, err = gzip.NewReader(reader)
 		if err != nil {
 			return nil, fmt.Errorf("failed to new gzip reader: %v", err)
 		}
+	case "br":
+		reader = brotli.NewReader(reader)
+	case "deflate":
+		reader = flate.NewReader(reader)
 	}
 
 	body, err := io.ReadAll(reader)
@@ -155,7 +162,7 @@ func readDecompressedBody(w pkgHTTP.Response) ([]byte, error) {
 	}
 
 	// if os.Getenv("DEBUG") != "" {
-	log.Infof("[RpcRespHandler] : DEBUG:%v, Content-Encoding: %v,  encoding is gzip: %v, decompressed body: %s", os.Getenv("DEBUG"), encoding, encoding == "gzip", string(body))
+	log.Infof("[RpcRespHandler] : DEBUG:%v, Content-Encoding: %v, decompressed body: %s", os.Getenv("DEBUG"), encoding, string(body))
 	// }
 
 	return body, nil
